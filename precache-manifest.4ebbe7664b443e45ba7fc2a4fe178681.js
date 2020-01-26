eaad5e5b2e539f690d556db65d5ea8f16319a104
Kattusha <kajra.life@gmail.com>
2020-01-27 00:25:51 +0300

    Updates

---
+++ precache-manifest.4ebbe7664b443e45ba7fc2a4fe178681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3413e9a83c2b1e7c1bd35c648a16f4d9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b3d68c0a7345e7b6eb02",
+    "url": "/portfolio/static/css/main.42718bbc.chunk.css"
+  },
+  {
+    "revision": "a97e8f89a3dd95be9465",
+    "url": "/portfolio/static/js/2.66c74f0f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.66c74f0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3d68c0a7345e7b6eb02",
+    "url": "/portfolio/static/js/main.c2fb6de5.chunk.js"
+  },
+  {
+    "revision": "ca0def9a93d0554ba433",
+    "url": "/portfolio/static/js/runtime-main.fca81fd4.js"
+  }
+]);
