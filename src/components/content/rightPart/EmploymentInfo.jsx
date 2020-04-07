3734375cd94c489693ad68299cabedd52cbf71f7
Kattusha <kajra.life@gmail.com>
2020-04-07 22:44:42 +0300

    add Employment part

--- src/components/content/rightPart/EmploymentInfo.jsx
+++ src/components/content/rightPart/EmploymentInfo.jsx
@@ -8,7 +8,32 @@ function EmploymentInfo() {
                 <h2>Employment</h2>
             </div>
             <div className="text">
-
+                <div className={styles.item}>
+                    <div className={styles.leftPartItem}>
+                        <div className={styles.yearsItem}>2010 - 2019</div>
+                    </div>
+                    <div className={styles.rightPartItem}>
+                        <h3>Front-end developer <br/>
+                            <span>startup BudgetMatador, Tallinn, Estonia</span>
+                        </h3>
+                        <p>Voluntarily helping a not-yet-funded start-up company to develop
+                            its product, for self-educational purposes. Includes coding in
+                            Javascript (Backbone, jQuery, Underscore), HTML and CSS.</p>
+                    </div>
+                </div>
+                <div className={styles.item}>
+                    <div className={styles.leftPartItem}>
+                        <div className={styles.yearsItem}>2010 - 2019</div>
+                    </div>
+                    <div className={styles.rightPartItem}>
+                        <h3>Front-end developer <br/>
+                            <span>startup BudgetMatador, Tallinn, Estonia</span>
+                        </h3>
+                        <p>Voluntarily helping a not-yet-funded start-up company to develop
+                            its product, for self-educational purposes. Includes coding in
+                            Javascript (Backbone, jQuery, Underscore), HTML and CSS.</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
