52042263bfd9d28fbb751acdc6d169c6097b7699
Kattusha <kajra.life@gmail.com>
2021-09-30 19:03:51 +0300

    test

--- src/components/content/leftPart/AboutMe.jsx
+++ src/components/content/leftPart/AboutMe.jsx
@@ -6,11 +6,12 @@ function AboutMe() {
     return (
         <div className="leftBox">
             <div className="title">
-                <h3>About Me</h3>
+                <h3>About Me Me Me Me</h3>
             </div>
             <div>
                 <p>I am a frontend developer interested in building usable, useful, well-constructed websites and
                     applications. Interested in continuation of learning and cooperation with excellent people.
+                    Interested in continuation of learning and cooperation with excellent people.
                 </p>
             </div>
             <div className={styles.signature}>
